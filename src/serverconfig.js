287c45e47190113ef69ba653230556883aef6710
Afzaal Ahmad Zeeshan <afzaal.ahmad.zeeshan@hotmail.com>
2018-04-18 17:24:41 +0500

    Worked with the ports and their configurations on different servers.

--- src/serverconfig.js
+++ src/serverconfig.js
@@ -1,9 +1,16 @@
 let port = process.env.PORT || process.env.PORT_AZURE || process.env.PORT_AWS || 80;
 
+// OpenShift ports
+if(process.env.OPENSHIFT_NODEJS_PORT) {
+    port = process.env.OPENSHIFT_NODEJS_PORT;
+}
+
+// Override the ports
 if(process.env.OVERRIDE_PORT) {
     port = process.env.CUSTOM_PORT;
 }
 
+// Variable name environment variable for the port
 if(process.env.VARIABLE_PORT) {
     port = process.env[process.env.VARIABLE_PORT_NAME];
 }
